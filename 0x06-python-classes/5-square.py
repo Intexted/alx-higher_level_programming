d6133c6eed0410e1a58a82392b55db5df2731200
intexted <lotfiaouane@gmail.com>
2023-06-27 06:34:24 -0700

    0x06-python-classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
